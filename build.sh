c0685890b705b0a7a3a561c79b8a2f459be751e7
Hrxander <hrtahsinhossain@gmail.com>
2026-05-01 20:09:14 +0600

    fix: remove compilemessage command from build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 set -o errexit  
 
 pip install -r requirements.txt
-sudo apt-get install -y gettext   
-python manage.py compilemessages 
 python manage.py collectstatic --noinput
 python manage.py migrate
--- sals_project/urls.py
+++ sals_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-
+import rosetta
 urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
